34a24341e9cc632b7cf190cb771ab692b83ff2a1
Frederik <frederik@pretzmann.dk>
2023-03-27 18:14:54 +0000

    Refined error handling & minor code changes

--- node/scripts/app_GE.js
+++ node/scripts/app_GE.js
@@ -1,35 +1,61 @@
 'use strict'
 
 function createTable(rows, colums) {
-    if(rows < 1 || colums < 1) {
-        throw new Error('Matrix size must be larger than 0');
+    try {   // If the matrix's size is smaller than 1 then ABORT
+        if(rows < 1 || colums < 1) {
+            throw new Error(`Matrix size must be larger than 0 (Got row = ${rows}, colum = ${colums})`);
+        }
+    } catch (error) {
+        console.error(`${error}`);
+        //return;   // Technically not necessary to stop code execution here since i and j starts at 1 down below
     }
 
-    const cell = document.createElement('input')
-    cell.style.width = '10px';
     const body = document.body,
-          tbl = document.createElement('table');
-    tbl.style.width = '200px';
-    //tbl.style.border = '1px solid black';
+          tbl = document.createElement('table');  
 
+    /************************
+        Control variables
+    ************************/
+    const cell_width = '100px',    // Width of input cells in the matrix
+          max_input_length = 15;   // Max number of chars allowed in input cells
+
+
+
+    /************************
+    ************************/
+
+    tbl.style.width = cell_width;
     for (let i = 1; i <= rows; i++) {    // Print rows
         const tr = tbl.insertRow();
         for (let j = 1; j <= colums; j++) {  // Print colums
-            if(i === 2 && j === 1) {
+            /*if(i === 2 && j === 1) {
                 //break;
-            }
-            if(true) {
-                let td = tr.insertCell();
-                td.appendChild(cell);
-                document.createElement(br);
-                //td.appendChild(document.createTextNode(`Cell ${i},${j}`));
-                //td.style.border = '1px solid black';
-                if(i === 1 && j === 1) {
-                    //td.setAttribute('rowSpan', '2');
-                }
+            }*/
+            
+            let td = tr.insertCell();
+            let input_cell = document.createElement('input');
+            input_cell.setAttribute("maxLength", max_input_length);
+            input_cell.style.width = cell_width;
+            td.appendChild(input_cell);
+            //td.appendChild(document.createTextNode(`Cell ${i},${j}`));
+            td.style.border = '1px solid black';
+            
+            /*if(i === 1 && j === 1) {
+                //td.setAttribute('rowSpan', '2');
+              }*/
             }
         }
-    }
     body.appendChild(tbl);
 }
+
 createTable(4,4);
+
+
+function getTable_size() {
+    const body = document.body;
+    const input = document.createElement('input');
+
+    
+
+
+}
