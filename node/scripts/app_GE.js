4d604afb462fc959be38c0cf78f13b5a6f353498
Frederik <frederik@pretzmann.dk>
2023-03-28 14:26:04 +0200

    Added user-specified, dynamic table gen

--- node/scripts/app_GE.js
+++ node/scripts/app_GE.js
@@ -11,15 +11,16 @@ function createTable(rows, colums) {
     }
 
     const body = document.body,
-          tbl = document.createElement('table');  
+          tbl = document.createElement('table');
+          tbl.setAttribute("id", "matrix");  
 
     /************************
         Control variables
     ************************/
     const cell_width = '100px',    // Width of input cells in the matrix
-          max_input_length = 5;   // Max number of chars allowed in input cells
+          max_input_length = 10;   // Max number of chars allowed in input cells
 
-
+    body.childNodes
 
     /************************
     ************************/
@@ -27,6 +28,8 @@ function createTable(rows, colums) {
     tbl.style.width = cell_width;
     for (let i = 1; i <= rows; i++) {    // Print rows
         const tr = tbl.insertRow();
+        tr.setAttribute("draggable", true); // Make rows draggable
+
         for (let j = 1; j <= colums; j++) {  // Print colums
             /*if(i === 2 && j === 1) {
                 //break;
@@ -55,30 +58,57 @@ function createTable(rows, colums) {
     body.appendChild(tbl);
 }
 
-createTable(4,4);
-
+function deleteTable() {
+    const table = document.getElementById("matrix");
+    const parent = table.parentElement;
+    parent.removeChild(table);
+    return;
+}
 
 function getTable_size() {
     const body = document.body;
-    const input = document.createElement('input');
+    const input_rows = document.createElement('input');
+    const input_colums = document.createElement('input');
+    const cdot = document.createTextNode(' x ');
+    let rows = 2;
+    let colums = 2;
+    let input_size = '25px';
     
-    input.setAttribute("id", "table_size");
-    //input.setAttribute("onclick", console.log('This works'));
+    input_rows.setAttribute("id", "row_size");
+    input_rows.setAttribute("title", "Input desired row size - max 10");
+    input_rows.setAttribute("value", rows);
+    input_rows.style.width = input_size;
+
+    input_colums.setAttribute("id", "colum_size");
+    input_colums.setAttribute("title", "Input desired colum size - max 10");
+    input_colums.setAttribute("value", colums);
+    input_colums.style.width = input_size;
+
+    body.appendChild(input_rows);    // When done editing the element, add it to the html body. This is crucial for stuff like getElementById
+    body.appendChild(input_colums);
+    body.insertBefore(cdot, input_colums);
 
 
-
-    body.appendChild(input);    // When done editing the element, add it to the html body. This is crucial for stuff like getElementById
-
-
-    const id = input.getAttribute("id");
-    let element = document.getElementById("table_size");
-
-    console.log(`Got element: ${element} and id: ${id}`);
+    //const id = input_rows.getAttribute("id");
+    let row_element = document.getElementById("row_size");
+    let colum_element = document.getElementById("colum_size");
+    //console.log(`Got row element: ${row_element} and id: ${id}`);
     
+    row_element.addEventListener("keypress", e => {
+        if(e.key === 'Enter') {
+            rows = row_element.value;
+            deleteTable();
+            createTable(rows, colums);
+        }});
 
+    colum_element.addEventListener("keypress", e => {
+        if(e.key === 'Enter') {
+            colums = colum_element.value;
+            deleteTable();
+            createTable(rows, colums);
+        }});
 
-
-
+    createTable(rows, colums);  // Initialize table
 }
 
 getTable_size();
